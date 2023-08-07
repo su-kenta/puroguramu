8011c026e0c2edaedfb6a9ac9002647649097f0a
su-kenta <139565871+su-kenta@users.noreply.github.com>
2023-08-07 23:56:26 +0900

    Add files via upload

--- Python.py
+++ Python.py
@@ -12,7 +12,7 @@ driver = webdriver.Chrome()
 #健康アンケート先を指定
 driver.get('http://example.ed.jp/port/login.php') 
 
-#待ち時間を0にする
+#ページを表示する猶予を設定
 time.sleep(0)
 #ログイン情報入力
 id = driver.find_element(By.XPATH, '//*[@id="UsrCode"]').send_keys(学籍番号) 
@@ -20,7 +20,7 @@ pwd = driver.find_element(By.XPATH,'//*[@id="PasCode"]').send_keys(誕生日)
 login = driver.find_element(By.XPATH, '//*[@id="form1"]/div[3]/table/tbody/tr[3]/td/input') 
 login.click()
 
-#ロード時間を0.1秒に設定
+#ページを表示する猶予を設定
 time.sleep(0.1)
 
 #1個目の質問を選択しクリック
@@ -39,4 +39,5 @@ time.sleep(0)
 #アンケートを送信
 answer = driver.find_element(By.XPATH, '//*[@id="form1"]/div[4]/div[27]/table/tbody/tr/td[2]/input') 
 answer.click()
+#ページを表示する猶予を設定
 time.sleep(2)
